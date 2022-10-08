1bff8376e2e0048984e29d5262367116daf1ee97
Dev XO <code.devxo@gmail.com>
2022-10-08 06:07:34 +0200

    Chore: add `Polyfills` comment

--- test/setup-test-env.ts
+++ test/setup-test-env.ts
@@ -1,4 +1,12 @@
 import { installGlobals } from '@remix-run/node'
 import '@testing-library/jest-dom/extend-expect'
 
+/**
+ * Since Remix relies on browser API's such as fetch,
+ * that are not natively available in Node.js,
+ * you may find that your unit tests fail without these globals,
+ * when running with tools such as Jest.
+ *
+ * Learn more about Remix Polyfills https://remix.run/docs/en/v1/other-api/node#polyfills
+ */
 installGlobals()
