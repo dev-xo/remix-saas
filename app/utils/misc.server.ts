164d4595039a3c5e60706140eea7d91c4bca6410
Dev XO <code.devxo@gmail.com>
2022-10-25 01:47:17 +0200

    Chore: update utils

--- app/utils/misc.server.ts
+++ app/utils/misc.server.ts
@@ -1,3 +1,23 @@
+/**
+ * This should be used any time the redirect path is user-provided.
+ * This avoids open-redirect vulnerabilities.
+ */
+export const safeRedirect = (
+	to: FormDataEntryValue | string | null | undefined,
+	defaultRedirect = '/',
+) => {
+	if (
+		!to ||
+		typeof to !== 'string' ||
+		!to.startsWith('/') ||
+		to.startsWith('//')
+	) {
+		return defaultRedirect
+	}
+
+	return to
+}
+
 export const getDomainUrl = (request: Request) => {
 	const host =
 		request.headers.get('X-Forwarded-Host') ?? request.headers.get('host')
--- app/utils/misc.ts
+++ app/utils/misc.ts
@@ -1,26 +1,6 @@
 import dayjs from 'dayjs'
 import IsSameOrAfter from 'dayjs/plugin/isSameOrAfter'
 
-/**
- * This should be used any time the redirect path is user-provided.
- * This avoids open-redirect vulnerabilities.
- */
-export const safeRedirect = (
-	to: FormDataEntryValue | string | null | undefined,
-	defaultRedirect = '/',
-) => {
-	if (
-		!to ||
-		typeof to !== 'string' ||
-		!to.startsWith('/') ||
-		to.startsWith('//')
-	) {
-		return defaultRedirect
-	}
-
-	return to
-}
-
 export const formatUnixDate = (unixDate: number) => {
 	if (typeof unixDate === 'number')
 		return dayjs.unix(unixDate).format('DD/MM/YYYY HH:mm')
