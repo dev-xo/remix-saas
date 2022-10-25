faa486048ca01042edd2f826efa3c52e62dba1a9
Dev XO <code.devxo@gmail.com>
2022-10-25 23:37:11 +0200

    Chore: cleanup /account page

--- app/routes/__app/account.tsx
+++ app/routes/__app/account.tsx
@@ -27,7 +27,7 @@ export const meta: MetaFunction = () => {
  * @required Template code.
  */
 type LoaderData = {
-	user: Awaited<AuthSession> | null
+	user: Awaited<AuthSession>
 	hasSuccessfullySubscribed: boolean | null
 	hasSuccessfullyUpdatedPlan: boolean | null
 	purchasedPlanName: string | number | null
@@ -40,6 +40,7 @@ export const loader = async ({ request }: LoaderArgs) => {
 	})
 
 	// Parses a Cookie and returns its associated Session.
+	// Gets values from Session.
 	const session = await getSession(request.headers.get('Cookie'))
 
 	const hasSuccessfullySubscribed =
@@ -61,10 +62,8 @@ export const loader = async ({ request }: LoaderArgs) => {
 		if (hasSubscriptionExpired) {
 			const subscription = await retrieveStripeSubscription(subscriptionId)
 
-			if (subscription && subscription?.status === 'canceled') {
+			if (subscription && subscription.status === 'canceled') {
 				let session = await getSession(request.headers.get('Cookie'))
-
-				console.log('BLOCK 2')
 
 				session.set(authenticator.sessionKey, {
 					...user,
@@ -113,213 +112,204 @@ export default function AccountRoute() {
 	return (
 		<div className="m-12 mx-auto flex h-full w-full max-w-4xl flex-col px-6 sm:flex-row">
 			{/* User Account. */}
-			{user && (
-				<div className="flex w-full flex-col items-center">
-					{/* Avatar. */}
-					<div className="relative flex flex-col">
-						{user.avatar && (
-							<img
-								src={user.avatar}
-								alt="Avatar"
-								className="h-44 w-44 select-none rounded-full shadow-xl transition hover:scale-110"
-							/>
-						)}
-					</div>
-					<div className="m-3" />
-
-					{/* Info. */}
-					<div className="flex flex-col items-center">
-						<h5 className="relative flex flex-row items-center text-3xl font-bold text-gray-900 dark:text-gray-100">
-							{user.name}
-							<svg
-								className="absolute -right-9 h-7 w-7 fill-sky-500"
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 24 24">
-								<path d="M19.965 8.521C19.988 8.347 20 8.173 20 8c0-2.379-2.143-4.288-4.521-3.965C14.786 2.802 13.466 2 12 2s-2.786.802-3.479 2.035C6.138 3.712 4 5.621 4 8c0 .173.012.347.035.521C2.802 9.215 2 10.535 2 12s.802 2.785 2.035 3.479A3.976 3.976 0 0 0 4 16c0 2.379 2.138 4.283 4.521 3.965C9.214 21.198 10.534 22 12 22s2.786-.802 3.479-2.035C17.857 20.283 20 18.379 20 16c0-.173-.012-.347-.035-.521C21.198 14.785 22 13.465 22 12s-.802-2.785-2.035-3.479zm-9.01 7.895-3.667-3.714 1.424-1.404 2.257 2.286 4.327-4.294 1.408 1.42-5.749 5.706z" />
-							</svg>
-						</h5>
-						<div className="mb-1" />
-						<span className="font-semibold text-gray-700 dark:text-gray-400">
-							My account
-						</span>
-					</div>
-					<div className="mb-3" />
-
-					{/* Renders `DeleteUserButton` button component. */}
-					<DeleteUserButton />
-				</div>
-			)}
-			<div className="mb-8" />
-
-			{/* Subscription Related. */}
-			{user && (
-				<div className="flex w-full flex-col items-center">
-					<div className="relative flex flex-row justify-center transition hover:scale-110">
+			<div className="flex w-full flex-col items-center">
+				{/* Avatar. */}
+				<div className="relative flex flex-col">
+					{user.avatar && (
 						<img
-							src="https://raw.githubusercontent.com/dev-xo/dev-xo/main/stripe-stack/assets/images/basket.png"
-							alt=""
-							className="h-44 w-44 select-none opacity-40 drop-shadow-xl"
+							src={user.avatar}
+							alt="Avatar"
+							className="h-44 w-44 select-none rounded-full shadow-xl transition hover:scale-110"
 						/>
+					)}
+				</div>
+				<div className="m-3" />
 
-						{/* Purchased Plans. */}
-						{!purchasedPlanName && (
-							<div className="absolute top-6 flex flex-row items-center justify-center">
-								<img
-									src="https://raw.githubusercontent.com/dev-xo/dev-xo/main/stripe-stack/assets/images/cookie.png"
-									alt=""
-									className="h-16 w-16 animate-pulse drop-shadow-lg"
-								/>
-							</div>
-						)}
-
-						{purchasedPlanName === 'Basic' && (
-							<div className="absolute top-6 flex flex-row items-center justify-center">
-								<img
-									src="https://raw.githubusercontent.com/dev-xo/dev-xo/main/stripe-stack/assets/images/cookie.png"
-									alt=""
-									className="h-16 w-16 animate-pulse drop-shadow-lg"
-								/>
-							</div>
-						)}
-
-						{purchasedPlanName === 'Creative' && (
-							<div className="absolute top-6 flex flex-row items-center justify-center">
-								<img
-									src="https://raw.githubusercontent.com/dev-xo/dev-xo/main/stripe-stack/assets/images/cookie.png"
-									alt=""
-									className="relative left-4 h-16 w-16 animate-pulse drop-shadow-lg"
-								/>
-								<img
-									src="https://raw.githubusercontent.com/dev-xo/dev-xo/main/stripe-stack/assets/images/cookie.png"
-									alt=""
-									className="h-16 w-16 animate-pulse drop-shadow-lg"
-								/>
-								<img
-									src="https://raw.githubusercontent.com/dev-xo/dev-xo/main/stripe-stack/assets/images/cookie.png"
-									alt=""
-									className="relative right-4 h-16 w-16 animate-pulse drop-shadow-lg"
-								/>
-							</div>
-						)}
-
-						{purchasedPlanName === 'PRO' && (
-							<div className="absolute top-6 flex flex-row items-center justify-center">
-								<img
-									src="https://raw.githubusercontent.com/dev-xo/dev-xo/main/stripe-stack/assets/images/cookie.png"
-									alt=""
-									className="relative left-10 h-16 w-16 animate-pulse drop-shadow-lg"
-								/>
-								<img
-									src="https://raw.githubusercontent.com/dev-xo/dev-xo/main/stripe-stack/assets/images/cookie.png"
-									alt=""
-									className="relative left-4 h-16 w-16 animate-pulse drop-shadow-lg"
-								/>
-								<img
-									src="https://raw.githubusercontent.com/dev-xo/dev-xo/main/stripe-stack/assets/images/cookie.png"
-									alt=""
-									className="h-16 w-16 animate-pulse drop-shadow-lg"
-								/>
-								<img
-									src="https://raw.githubusercontent.com/dev-xo/dev-xo/main/stripe-stack/assets/images/cookie.png"
-									alt=""
-									className="relative right-4 h-16 w-16 animate-pulse drop-shadow-lg"
-								/>
-								<img
-									src="https://raw.githubusercontent.com/dev-xo/dev-xo/main/stripe-stack/assets/images/cookie.png"
-									alt=""
-									className="relative right-10 h-16 w-16 animate-pulse drop-shadow-lg"
-								/>
-							</div>
-						)}
-					</div>
-					<div className="m-3" />
-
-					{/* Displays Subscription Plan. */}
-					<h5 className="text-3xl font-bold text-gray-900 dark:text-gray-100">
-						{purchasedPlanName ? purchasedPlanName + ' Plan' : 'Subscription'}
+				{/* Info. */}
+				<div className="flex flex-col items-center">
+					<h5 className="relative flex flex-row items-center text-3xl font-bold text-gray-900 dark:text-gray-100">
+						{user.name}
+						<svg
+							className="absolute -right-9 h-7 w-7 fill-sky-500"
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24">
+							<path d="M19.965 8.521C19.988 8.347 20 8.173 20 8c0-2.379-2.143-4.288-4.521-3.965C14.786 2.802 13.466 2 12 2s-2.786.802-3.479 2.035C6.138 3.712 4 5.621 4 8c0 .173.012.347.035.521C2.802 9.215 2 10.535 2 12s.802 2.785 2.035 3.479A3.976 3.976 0 0 0 4 16c0 2.379 2.138 4.283 4.521 3.965C9.214 21.198 10.534 22 12 22s2.786-.802 3.479-2.035C17.857 20.283 20 18.379 20 16c0-.173-.012-.347-.035-.521C21.198 14.785 22 13.465 22 12s-.802-2.785-2.035-3.479zm-9.01 7.895-3.667-3.714 1.424-1.404 2.257 2.286 4.327-4.294 1.408 1.42-5.749 5.706z" />
+						</svg>
 					</h5>
 					<div className="mb-1" />
+					<span className="font-semibold text-gray-700 dark:text-gray-400">
+						My account
+					</span>
+				</div>
+				<div className="mb-3" />
 
-					{/* Displays a message if there is no Subscription. */}
-					{!user.subscription?.subscriptionId && (
-						<>
-							<p className="text-center font-semibold text-gray-500 dark:text-gray-400">
-								Subscribe to get some tasty Cookies!
-							</p>
-							<div className="mb-3" />
-						</>
-					)}
+				{/* Renders `DeleteUserButton` button component. */}
+				<DeleteUserButton />
+			</div>
+			<div className="mb-8" />
 
-					{/* Displays formated Subscription expiration date. */}
-					{user.subscription?.subscriptionId && (
-						<>
-							<p className="font-bold text-gray-700 dark:text-gray-400">
-								{user.subscription?.cancelAtPeriodEnd === true ? (
-									<span className="text-red-500">EXPIRES</span>
-								) : (
-									<span className="text-gray-900 dark:text-gray-100">
-										RENEWS
-									</span>
-								)}
-								:{' '}
-								{user.subscription.currentPeriodEnd &&
-									formatUnixDate(user.subscription.currentPeriodEnd)}
-							</p>
-							<div className="mb-3" />
-						</>
-					)}
+			{/* Subscription. */}
+			<div className="flex w-full flex-col items-center">
+				<div className="relative flex flex-row justify-center transition hover:scale-110">
+					<img
+						src="https://raw.githubusercontent.com/dev-xo/dev-xo/main/stripe-stack/assets/images/basket.png"
+						alt=""
+						className="h-44 w-44 select-none opacity-40 drop-shadow-xl"
+					/>
 
-					{/* Displays a message if Subscription renew has been cancelled. */}
-					{user.subscription?.cancelAtPeriodEnd === true && (
-						<>
-							<p className="text-center font-semibold text-gray-500 dark:text-gray-400">
-								Your plan will be cancelled but your subscription benefits will
-								still active until expiration date.
-							</p>
-							<div className="mb-3" />
-						</>
-					)}
-
-					{/* Displays a Link Button to `/plans` route. */}
-					<Link to="/plans">
-						<button className="flex h-9 flex-row items-center justify-center rounded-xl bg-violet-500 px-12 text-base font-bold text-white transition hover:scale-105 active:scale-100">
-							<span>
-								{!user.subscription?.subscriptionId ? 'Subscribe' : 'Plans'}
-							</span>
-						</button>
-					</Link>
-
-					{/* Renders `CreateCustomerPortalButton` component. */}
-					{user.subscription?.customerId && (
-						<>
-							<div className="mb-3" />
-							<CreateCustomerPortalButton />
-						</>
-					)}
-
-					{/* Displays Checkout Success Message. */}
-					{hasSuccessfullySubscribed && (
-						<div
-							className="select-noneflex-row absolute top-9 left-0 right-0 z-20 m-auto flex
-							w-[300px] transform justify-center transition hover:scale-110">
-							<p className="rounded-2xl bg-violet-500 p-2 px-12 font-bold text-white shadow-2xl">
-								Successfully Subscribed
-							</p>
+					{/* Purchased Plans. */}
+					{!purchasedPlanName && (
+						<div className="absolute top-6 flex flex-row items-center justify-center">
+							<img
+								src="https://raw.githubusercontent.com/dev-xo/dev-xo/main/stripe-stack/assets/images/cookie.png"
+								alt=""
+								className="h-16 w-16 animate-pulse drop-shadow-lg"
+							/>
 						</div>
 					)}
 
-					{/* Displays Updated Plan Success Message. */}
-					{hasSuccessfullyUpdatedPlan && (
-						<div
-							className="select-noneflex-row absolute top-9 left-0 right-0 z-20 m-auto flex
-							w-[400px] transform justify-center transition hover:scale-110">
-							<p className="rounded-2xl bg-violet-500 p-2 px-12 font-bold text-white shadow-2xl">
-								Plan has been Successfully Updated.
-							</p>
+					{purchasedPlanName === 'Basic' && (
+						<div className="absolute top-6 flex flex-row items-center justify-center">
+							<img
+								src="https://raw.githubusercontent.com/dev-xo/dev-xo/main/stripe-stack/assets/images/cookie.png"
+								alt=""
+								className="h-16 w-16 animate-pulse drop-shadow-lg"
+							/>
+						</div>
+					)}
+
+					{purchasedPlanName === 'Creative' && (
+						<div className="absolute top-6 flex flex-row items-center justify-center">
+							<img
+								src="https://raw.githubusercontent.com/dev-xo/dev-xo/main/stripe-stack/assets/images/cookie.png"
+								alt=""
+								className="relative left-4 h-16 w-16 animate-pulse drop-shadow-lg"
+							/>
+							<img
+								src="https://raw.githubusercontent.com/dev-xo/dev-xo/main/stripe-stack/assets/images/cookie.png"
+								alt=""
+								className="h-16 w-16 animate-pulse drop-shadow-lg"
+							/>
+							<img
+								src="https://raw.githubusercontent.com/dev-xo/dev-xo/main/stripe-stack/assets/images/cookie.png"
+								alt=""
+								className="relative right-4 h-16 w-16 animate-pulse drop-shadow-lg"
+							/>
+						</div>
+					)}
+
+					{purchasedPlanName === 'PRO' && (
+						<div className="absolute top-6 flex flex-row items-center justify-center">
+							<img
+								src="https://raw.githubusercontent.com/dev-xo/dev-xo/main/stripe-stack/assets/images/cookie.png"
+								alt=""
+								className="relative left-10 h-16 w-16 animate-pulse drop-shadow-lg"
+							/>
+							<img
+								src="https://raw.githubusercontent.com/dev-xo/dev-xo/main/stripe-stack/assets/images/cookie.png"
+								alt=""
+								className="relative left-4 h-16 w-16 animate-pulse drop-shadow-lg"
+							/>
+							<img
+								src="https://raw.githubusercontent.com/dev-xo/dev-xo/main/stripe-stack/assets/images/cookie.png"
+								alt=""
+								className="h-16 w-16 animate-pulse drop-shadow-lg"
+							/>
+							<img
+								src="https://raw.githubusercontent.com/dev-xo/dev-xo/main/stripe-stack/assets/images/cookie.png"
+								alt=""
+								className="relative right-4 h-16 w-16 animate-pulse drop-shadow-lg"
+							/>
+							<img
+								src="https://raw.githubusercontent.com/dev-xo/dev-xo/main/stripe-stack/assets/images/cookie.png"
+								alt=""
+								className="relative right-10 h-16 w-16 animate-pulse drop-shadow-lg"
+							/>
 						</div>
 					)}
 				</div>
-			)}
+				<div className="m-3" />
+
+				{/* Displays Subscription Plan. */}
+				<h5 className="text-3xl font-bold text-gray-900 dark:text-gray-100">
+					{purchasedPlanName ? purchasedPlanName + ' Plan' : 'Subscription'}
+				</h5>
+				<div className="mb-1" />
+
+				{/* Displays a message if User has no Subscription. */}
+				{!user.subscription?.subscriptionId && (
+					<>
+						<p className="text-center font-semibold text-gray-500 dark:text-gray-400">
+							Subscribe to get some tasty Cookies!
+						</p>
+						<div className="mb-3" />
+					</>
+				)}
+
+				{/* Displays formated Subscription expiration date. */}
+				{user.subscription?.subscriptionId && (
+					<>
+						<p className="font-bold text-gray-700 dark:text-gray-400">
+							{user.subscription?.cancelAtPeriodEnd === true ? (
+								<span className="text-red-500">EXPIRES</span>
+							) : (
+								<span className="text-gray-900 dark:text-gray-100">RENEWS</span>
+							)}
+							:{' '}
+							{user.subscription.currentPeriodEnd &&
+								formatUnixDate(user.subscription.currentPeriodEnd)}
+						</p>
+						<div className="mb-3" />
+					</>
+				)}
+
+				{/* Displays a message if Subscription renew has been cancelled. */}
+				{user.subscription?.cancelAtPeriodEnd === true && (
+					<>
+						<p className="text-center font-semibold text-gray-500 dark:text-gray-400">
+							Your plan will be cancelled but your subscription benefits will
+							still active until expiration date.
+						</p>
+						<div className="mb-3" />
+					</>
+				)}
+
+				{/* Displays a Link to `/plans` route. */}
+				<Link
+					to="/plans"
+					className="flex h-9 flex-row items-center justify-center rounded-xl bg-violet-500 
+					px-12 text-base font-bold text-white transition hover:scale-105 active:scale-100">
+					<span>
+						{!user.subscription?.subscriptionId ? 'Subscribe' : 'Plans'}
+					</span>
+				</Link>
+				<div className="mb-3" />
+
+				{/* On Customer, Renders `CreateCustomerPortalButton` component. */}
+				{user.subscription?.customerId && <CreateCustomerPortalButton />}
+
+				{/* Displays Checkout Success Message. */}
+				{hasSuccessfullySubscribed && (
+					<div
+						className="select-noneflex-row absolute top-9 left-0 right-0 z-20 m-auto flex
+						w-[300px] transform justify-center transition hover:scale-110">
+						<p className="rounded-2xl bg-violet-500 p-2 px-12 font-bold text-white shadow-2xl">
+							Successfully Subscribed
+						</p>
+					</div>
+				)}
+
+				{/* Displays Updated Plan Success Message. */}
+				{hasSuccessfullyUpdatedPlan && (
+					<div
+						className="select-noneflex-row absolute top-9 left-0 right-0 z-20 m-auto flex
+						w-[400px] transform justify-center transition hover:scale-110">
+						<p className="rounded-2xl bg-violet-500 p-2 px-12 font-bold text-white shadow-2xl">
+							Plan has been Successfully Updated.
+						</p>
+					</div>
+				)}
+			</div>
 		</div>
 	)
 }
