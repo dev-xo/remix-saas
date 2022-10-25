02ce81e519d4f82b0d14a773d1258de926cf0d66
Dev XO <code.devxo@gmail.com>
2022-10-25 04:24:01 +0200

    Chore: update index page icons

--- app/routes/__app/index.tsx
+++ app/routes/__app/index.tsx
@@ -168,19 +168,9 @@ export default function AppIndexRoute() {
 						href: 'https://fly.io',
 					},
 					{
-						src: 'https://user-images.githubusercontent.com/1500684/157764395-137ec949-382c-43bd-a3c0-0cb8cb22e22d.svg',
-						alt: 'SQLite',
-						href: 'https://sqlite.org',
-					},
-					{
 						src: 'https://user-images.githubusercontent.com/1500684/157764484-ad64a21a-d7fb-47e3-8669-ec046da20c1f.svg',
 						alt: 'Prisma',
 						href: 'https://prisma.io',
-					},
-					{
-						src: 'https://avatars.githubusercontent.com/u/44036562?s=280&v=4',
-						alt: 'Github Actions',
-						href: 'https://github.com/features/actions',
 					},
 					{
 						src: 'https://playwright.dev/img/playwright-logo.svg',
@@ -206,16 +196,6 @@ export default function AppIndexRoute() {
 						src: 'https://user-images.githubusercontent.com/1500684/157764276-a516a239-e377-4a20-b44a-0ac7b65c8c14.svg',
 						alt: 'Tailwind',
 						href: 'https://tailwindcss.com',
-					},
-					{
-						src: 'https://user-images.githubusercontent.com/1500684/157772934-ce0a943d-e9d0-40f8-97f3-f464c0811643.svg',
-						alt: 'Prettier',
-						href: 'https://prettier.io',
-					},
-					{
-						src: 'https://user-images.githubusercontent.com/1500684/157772990-3968ff7c-b551-4c55-a25c-046a32709a8e.svg',
-						alt: 'ESLint',
-						href: 'https://eslint.org',
 					},
 					{
 						src: 'https://user-images.githubusercontent.com/1500684/157773063-20a0ed64-b9f8-4e0b-9d1e-0b65a3d4a6db.svg',
