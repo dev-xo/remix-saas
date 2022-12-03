74121afac170d80d539e16f5791eb63e14b23db3
Dev XO <code.devxo@gmail.com>
2022-12-03 22:26:31 +0100

    Chore: update utils
    - Improved shared envs comments.

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -8,12 +8,7 @@ export function safeRedirect(
 	to: FormDataEntryValue | string | null | undefined,
 	defaultRedirect = '/',
 ) {
-	if (
-		!to ||
-		typeof to !== 'string' ||
-		!to.startsWith('/') ||
-		to.startsWith('//')
-	) {
+	if (!to || typeof to !== 'string' || !to.startsWith('/') || to.startsWith('//')) {
 		return defaultRedirect
 	}
 
@@ -21,8 +16,7 @@ export function safeRedirect(
 }
 
 export function getDomainUrl(request: Request) {
-	const host =
-		request.headers.get('X-Forwarded-Host') ?? request.headers.get('host')
+	const host = request.headers.get('X-Forwarded-Host') ?? request.headers.get('host')
 	if (!host) throw new Error('Could not determine domain URL.')
 
 	const protocol = host.includes('localhost') ? 'http' : 'https'
@@ -51,7 +45,11 @@ export function hasDateExpired(date: number) {
 }
 
 /**
- * Global Envs Utils.
+ * Global Envs.
+ *
+ * Shared envs are used in both `entry.server.ts` and `root.tsx`.
+ * Do not share sensible variables that you do not wish to
+ * be included in the client.
  */
 export function getGlobalEnvs() {
 	return {
