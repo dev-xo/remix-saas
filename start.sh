1b32121ba8e547dff793ded10b7f7e8b2fd980b3
Michaël De Boey <info@michaeldeboey.be>
2023-11-07 01:20:17 +0100

    fix: add swapfile (#135)

--- start.sh
+++ start.sh
@@ -1,8 +1,17 @@
-# This file is how Fly starts the server (configured in fly.toml). 
-# Before starting the server, we need to run any prisma migrations that haven't yet been run.
+#!/bin/sh -ex
+
+# This file is how Fly starts the server (configured in fly.toml). Before starting
+# the server though, we need to run any prisma migrations that haven't yet been
+# run, which is why this file exists in the first place.
 # Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
 
-set -ex
+# allocate swap space
+fallocate -l 512M /swapfile
+chmod 0600 /swapfile
+mkswap /swapfile
+echo 10 > /proc/sys/vm/swappiness
+swapon /swapfile
+echo 1 > /proc/sys/vm/overcommit_memory
+
 npx prisma migrate deploy
-npx node prisma/seed/prisma/seed.js
 npm run start
