09f7331deca21afafce07e23687c838db80f562b
Youssef Adbib <mradbib@gmail.com>
2024-05-16 15:17:40 +0100

    add Logo import

--- app/routes/_home+/_index.tsx
+++ app/routes/_home+/_index.tsx
@@ -10,6 +10,8 @@ import { ROUTE_PATH as LOGIN_PATH } from '#app/routes/auth+/login'
 import { Button, buttonVariants } from '#app/components/ui/button'
 import { ThemeSwitcherHome } from '#app/components/misc/theme-switcher'
 import ShadowPNG from '#public/images/shadow.png'
+import Logo from '#app/components/logo'
+
 
 export const meta: MetaFunction = () => {
   return [{ title: `${siteConfig.siteTitle} - Starter Kit` }]
