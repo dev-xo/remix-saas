7e3684e32f9f4f56cacc91d3f6fbec05e2d20a86
dev-xo <devxo@mail.com>
2024-08-10 07:16:52 +0200

    chore(deps): bump stripe from 15.10.0 to 16.6.0

--- app/modules/stripe/stripe.server.ts
+++ app/modules/stripe/stripe.server.ts
@@ -6,6 +6,6 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2024-04-10',
+  apiVersion: '2024-06-20',
   typescript: true,
 })
